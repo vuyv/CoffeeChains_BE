2966c041fd6f9d7c9b4aac9a0c49518dc78ba904
vuyv <vuyv3103@gmail.com>
2022-04-27 09:17:59 +0700

    Edit Manager Report

--- src/main/java/com/enclave/backend/api/ReportAPI.java
+++ src/main/java/com/enclave/backend/api/ReportAPI.java
@@ -12,36 +12,6 @@ public class ReportAPI {
     @Autowired
     private ReportService reportService;
 
-    @GetMapping("/seller/{branchId}/daily/{date}")
-    public List<Object[]> getTopSellerDaily(@PathVariable("branchId") short branchId, @PathVariable("date") String date) {
-        return reportService.getTopSellerDaily(branchId, date);
-    }
-
-    @GetMapping("/seller/{branchId}/{type}/{date}")
-    public List<Object[]> getTopSeller(@PathVariable("branchId") short branchId, @PathVariable("date") String date, @PathVariable("type") String type) {
-        return reportService.getTopSeller(branchId, date, type);
-    }
-
-    @GetMapping("/product/all/{branchId}/{type}/{date}")
-    public List<Object[]> getBestSellingAllCategory(@PathVariable("branchId") short branchId, @PathVariable("date") String date, @PathVariable("type") String type) {
-        return reportService.getBestSellingAllCategory(branchId, date, type);
-    }
-
-    @GetMapping("/product/all/{branchId}/daily/{date}")
-    public List<Object[]> getBestSellingAllCategoryDaily(@PathVariable("branchId") short branchId, @PathVariable("date") String date) {
-        return reportService.getBestSellingAllCategoryDaily(branchId, date);
-    }
-
-    @GetMapping("/product/{categoryId}/{branchId}/{type}/{date}")
-    public List<Object[]> getBestSellingByCategory(@PathVariable("branchId") short branchId,@PathVariable("categoryId") short categoryId, @PathVariable("date") String date, @PathVariable("type") String type) {
-        return reportService.getBestSellingByCategory(branchId,categoryId, date, type);
-    }
-
-    @GetMapping("/product/{categoryId}/{branchId}/daily/{date}")
-    public List<Object[]> getBestSellingByCategoryDaily(@PathVariable("branchId") short branchId,@PathVariable("categoryId") short categoryId, @PathVariable("date") String date) {
-        return reportService.getBestSellingByCategoryDaily(branchId,categoryId, date);
-    }
-
     //OWNER
     //SUPER CLEAR & CLEAN
     @GetMapping("/owner")
@@ -53,6 +23,18 @@ public class ReportAPI {
             return reportService.getProductEachCategoryByTime(category, date, timeRange);
         }
         return reportService.getRevenueAllBranchByTime(date, timeRange);
+
+    }
+
+    @GetMapping("/manager")
+    public List<Object[]> getReportByBranch(@RequestParam(name="type") String type, @RequestParam(name="branch") short branch, @RequestParam(name="category") short category, @RequestParam(name="timeRange") String timeRange,@RequestParam(name="date") String date ) {
+        if (type.equals("Product") && category == 0) {
+            return reportService.getProductsAllCategoryEachBranch(branch, date, timeRange);
+        }
+        if (type.equals("Product") && category != 0) {
+            return reportService.getProductsByCategoryEachBranch(branch, category, date, timeRange);
+        }
+        return reportService.getEmployeesEachBranch(branch,date, timeRange);
 
     }
 }
--- src/main/java/com/enclave/backend/service/ReportService.java
+++ src/main/java/com/enclave/backend/service/ReportService.java
@@ -1,23 +1,8 @@
 package com.enclave.backend.service;
 
-import io.lettuce.core.dynamic.annotation.Param;
-
 import java.util.List;
 
 public interface ReportService {
-
-    List<Object[]> getTopSeller(short branchId,String date, String type);
-
-    List<Object[]> getTopSellerDaily(short branchId,String date);
-
-    List<Object[]> getBestSellingAllCategory(short branchId, String date, String type);
-
-    List<Object[]> getBestSellingAllCategoryDaily(short branchId, String date);
-
-    List<Object[]> getBestSellingByCategory(short branchId, short categoryId, String date, String type);
-
-    List<Object[]> getBestSellingByCategoryDaily(short branchId, short categoryId, String date);
-
 
     //OWNER REPORT
     //revenue
@@ -28,4 +13,15 @@ public interface ReportService {
 
     //eachCategory
     List<Object[]> getProductEachCategoryByTime(short categoryId, String date, String type);
+
+    //MANAGER REPORT
+
+    //allCategory
+    List<Object[]> getProductsAllCategoryEachBranch(short branchId, String date, String type);
+
+    //eachCategory
+    List<Object[]> getProductsByCategoryEachBranch(short branchId, short categoryId, String date, String type);
+
+    // Seller
+    List<Object[]> getEmployeesEachBranch(short branchId, String date, String type);
 }
